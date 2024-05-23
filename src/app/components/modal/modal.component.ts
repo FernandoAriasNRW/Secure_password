40696d37854ed1eb7b39f2f7789256c5aaf247d4
ferwindev <fernando.arias@numrot.com>
2024-05-23 17:02:42 -0500

    Changing the styles, working on the modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -31,13 +31,28 @@ export class ModalComponent implements OnInit {
 
     if (modal) {
       modal.style.display="block";
+      this.disableBackgroundInteractions();
     }
+  }
+
+  disableBackgroundInteractions() {
+    const body = document.querySelector('body');
+    const modal = document.getElementById('modal');
+
+    if (body && modal) {
+      body.style.pointerEvents = "none";
+      modal.style.pointerEvents = "auto";
+    }
+
   }
 
   closeModal(){
     let modal = document.getElementById('modal');
-    if (modal) {
+    const body = document.querySelector('body');
+
+    if (modal && body) {
       modal.style.display="none";
+      body.style.pointerEvents = "auto";
     }
   }
 
